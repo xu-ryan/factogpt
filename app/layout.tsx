f62bb8ee4502f13deed90da5ac6921cf85c2af44
Ryan <xu.ryan@outlook.com>
2023-07-04 13:23:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "FactoGPT",
+  description: "Your efficient AI assistant. Powered by Facto.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "FactoGPT",
     statusBarStyle: "default",
   },
 };
@@ -32,6 +32,8 @@ export default function RootLayout({
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
+        <script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js" defer></script>
+        <script src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js" defer></script>
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
       <body>{children}</body>
